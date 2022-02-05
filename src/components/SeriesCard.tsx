f2234753a587897d013beaab81dfa7f756e765bf
Balazs Nyirfa <balazsnyirfa@gmail.com>
2022-02-06 00:03:09 +0100

    make LibraryPage & SeriesCard responsive

--- src/components/SeriesCard.tsx
+++ src/components/SeriesCard.tsx
@@ -1,44 +1,39 @@
 import { Link } from "react-router-dom";
 import { Series } from "./../models/SeriesModel";
 import "./../style/SeriesCard.scss";
-import './../style/Neon.scss';
-
 
 const SeriesCard = (props: { series: Series }) => {
   return (
-    <div>
-      <div className="card mb-3 primary-light-neon">
-        <img src={props.series.thumb} className="card-img-top" alt={`${props.series.name}__thumb`} />
-        <div className="card-body secondary-dark-bg">
-          <div className="container">
-            <div className="row">
-              <div className="col">
-                <h5 className="card-title">{props.series.name}</h5>
-              </div>
+    <div className="card secondary-dark-bg mb-3 shadow-lg w-100 h-100 rounded-3">
+      <img
+        src={props.series.thumb}
+        className="img-fluid rounded-3"
+        alt={`${props.series.name}__thumb`}
+      />
+      <div className="card-body">
+        <div className="container-fluid">
+          <div className="row">
+            <div className="col">
+              <h5 className="card-title">{props.series.name}</h5>
             </div>
-            <div className="row">
-              <div className="col">
-                <p className="card-text">{props.series.description}</p>
-              </div>
+          </div>
+          <div className="row mt-3">
+            <div className="col">
+              <p className="card-text">
+                Uploaded by:{" "}
+                <span className="text-muted">
+                  <i>{props.series.uploadedBy.userName}</i>
+                </span>
+              </p>
             </div>
-            <div className="row mt-3">
-              <div className="col">
-                <p className="card-text">
-                  Uploaded by:{" "}
-                  <span className="text-muted">
-                    <i>{props.series.uploadedBy.userName}</i>
-                  </span>
-                </p>
-              </div>
-            </div>
-            <div className="row mt-5">
-              <div className="col d-flex justify-content-center align-content-center">
-                <Link to={`/episodes/${props.series._id}`}>
-                  <button className="btn secondary-light-bg text-white larger-text">
-                    Watch
-                  </button>
-                </Link>
-              </div>
+          </div>
+          <div className="row mt-3">
+            <div className="col d-flex justify-content-center align-content-center">
+              <Link to={`/episodes/${props.series._id}`}>
+                <button className="btn secondary-light-bg text-white larger-text">
+                  Watch
+                </button>
+              </Link>
             </div>
           </div>
         </div>
