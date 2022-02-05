cf1ec659c0141e28353ddc1733d943f91c5c7f1d
Balazs Nyirfa <balazsnyirfa@gmail.com>
2022-02-06 00:05:20 +0100

    extend Status component with errorMessage & colored fonts

--- src/components/Status.tsx
+++ src/components/Status.tsx
@@ -1,21 +1,27 @@
 const Status = (props: {
   isLoading: boolean;
   isError: boolean;
+  errorMessage?: string;
 }) => {
   return (
-    <div>
-      {props.isError === true && (
+    <div className="w-100">
+      {(props.isError === true && !props.errorMessage) && (
         <div>
-          <p>Error!</p>
+          <p className="text-danger">Error!</p>
         </div>
       )}
-      {(props.isError === false && props.isLoading === true) && (
+      {props.isError && props.errorMessage && (
+        <div>
+          <p className="text-danger">Error!<br/>{props.errorMessage}</p>
+        </div>
+      )}
+      {props.isError === false && props.isLoading === true && (
         <div>
           <p>Loading...</p>
         </div>
       )}
     </div>
-  )
-}
+  );
+};
 
 export default Status;
